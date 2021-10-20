bd2bfe80106ee2b6d1430129d39cd18a6694275e
cubodado <dalkang000@gmail.com>
2021-10-20 16:11:15 +0900

    style: apply css

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,15 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
-  return <h2>Expense item!</h2>
+  return (
+    <div className="expense-item">
+      <div>Date : Dec 1st 2021</div>
+      <div className="expense-item__description">
+        <h2>Title : Coffee</h2>
+        <div className="expense-item__price">Amount : $4</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
